9e999a862feee5a242e015ee4580a27d5f71442b
KanazawaRyo <bear_star713@hotmail.com>
2021-04-26 10:25:53 +0900

    initial

---
+++ nuxt.config.js
@@ -0,0 +1,53 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "~/assets/styles/main.css"
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [{
+    src: '~/plugins/vuex-persist',
+    mode: 'client'
+  }],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/me/about.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="home">
+    <h1 class="home__heading">About Nuxtjs Directory Structure</h1>
+    <div class="directories">
+      <div
+        class="directory__container"
+        v-for="directory in directories"
+        :key="directory.id"
+        @click.prevent="storeDirectoryInfo(directory)"
+      >
+        <p class="directory__name">
+          <nuxt-link
+            :to="{ name: 'id', params: { id: directory.id, dir: directory } }"
+          >{{ directory.name }}</nuxt-link>
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "about-nuxt",
+  data() {
+    return {
+      directories: [
+        //remains the same
+      ]
+    };
+  },
+  methods: {
+    storeDirectoryInfo(dir) {
+      this.$store.commit("saveInfo", {
+        directory: dir
+      });
+    }
+  }
+};
+</script>
+<style>
+</style>
